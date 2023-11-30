79efc0ea8976c815262468fc1b2a646c9f2cf09f
Pvp-man51 <93683710+Pvp-man51@users.noreply.github.com>
2023-11-30 20:20:55 +0100

    Fixed belt rendering bug

--- src/main/java/com/pvpman/snowdownessentails/item/client/GoldArmorRenderer.java
+++ src/main/java/com/pvpman/snowdownessentails/item/client/GoldArmorRenderer.java
@@ -1,11 +1,44 @@
 package com.pvpman.snowdownessentails.item.client;
 
 import com.pvpman.snowdownessentails.item.custom.GoldArmorItem;
+import net.minecraft.entity.EquipmentSlot;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class GoldArmorRenderer extends GeoArmorRenderer<GoldArmorItem> {
 
     public GoldArmorRenderer() {
         super(new GoldArmorModel());
+    }
+
+    @Override
+    protected void applyBoneVisibilityBySlot(EquipmentSlot currentSlot) {
+        setVisible(false);
+
+        switch (currentSlot) {
+            case HEAD -> {
+                setBoneVisible(this.head, true);
+            }
+            case CHEST -> {
+                setBoneVisible(this.body, true);
+                setBoneVisible(this.rightArm, true);
+                setBoneVisible(this.leftArm, true);
+                setBoneVisible(this.getGeoModel().getBone("patronengurt").get(), true);
+
+                setBoneVisible(this.getGeoModel().getBone("beltArmor").get(), false);
+            }
+            case LEGS -> {
+                setBoneVisible(this.body, true);
+                setBoneVisible(this.getGeoModel().getBone("beltArmor").get(), true);
+
+                setBoneVisible(this.rightLeg, true);
+                setBoneVisible(this.leftLeg, true);
+
+                setBoneVisible(this.getGeoModel().getBone("patronengurt").get(), false);
+            }
+            case FEET -> {
+                setBoneVisible(this.rightBoot, true);
+                setBoneVisible(this.leftBoot, true);
+            }
+        }
     }
 }
